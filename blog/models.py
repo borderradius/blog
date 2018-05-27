854bf1b3a1e27d1b8f9fd53c40c577602dd8dc65
spacemonkey <spacemonkey@spacemonkeyui-MacBook-Pro.local>
2018-05-27 16:59:36 +0900

    post 모델생성/마이그레이션, admin 등록

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,17 @@
 from django.db import models
 
 # Create your models here.
-class post(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=100) # 길이제한이 있는 문자열
     content = models.TextField() # 길이제한이 없는 문자열
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True) # auto_now_add 최초생성시 자동으로 들어감.
+    updated_at = models.DateTimeField(auto_now=True) # auto_now 레코드 생성시 자동으로 들어감.
+
+    # choices
+    # title1 = models.CharField(max_length=100,
+    #     choices = (
+    #         ('디비에 저장될 값1', 'ui에 보여질 레이블'),
+    #         ('디비에 저장될 값2', 'ui에 보여질 레이블'),
+    #         ('디비에 저장될 값3', 'ui에 보여질 레이블'),
+    #     ))
+    
