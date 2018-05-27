3bc23eb4c65af757989587aeafdc3e1e3766a253
spacemonkey <spacemonkey@spacemonkeyui-MacBook-Pro.local>
2018-05-27 17:16:08 +0900

    admin post 등록시 유효성 검사

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,24 @@
 # blog/models.py
+import re
+from django.forms import ValidationError
 from django.db import models
 
-# Create your models here.
+# [+_]?  정규표현식에서 ?는 0회 혹은 1회 출현을 의미함. 없거나 혹은 한번 나오거나. 
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
+
 class Post(models.Model):
     title = models.CharField(max_length=100) # 길이제한이 있는 문자열
     content = models.TextField() # 길이제한이 없는 문자열
+    tags = models.CharField(
+        max_length=100, 
+        blank=True)
+    lnglat = models.CharField(
+        max_length=50, 
+        blank=True, # 값이 없어도 저장됌. 필수값이 아니라는 뜻.
+        help_text='경도, 위도 포맷으로 입력',
+        validators=[lnglat_validator])
     created_at = models.DateTimeField(auto_now_add=True) # auto_now_add 최초생성시 자동으로 들어감.
     updated_at = models.DateTimeField(auto_now=True) # auto_now 레코드 생성시 자동으로 들어감.
 
