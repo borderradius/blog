75e15c7b38e19232b0efc4bbfa6203120d521d76
spacemonkey <spacemonkey@spacemonkeyui-MacBook-Pro.local>
2018-06-01 13:19:20 +0900

    settings.AUTH_USER_MODEL 올바른 지정

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,14 @@
 # accounts/models.py
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 class Profile(models.Model):
-    # user = models.ForeignKey('auth.User')
-    user = models.OneToOneField(User) # FIXME Bad case!!
+    # user = models.ForeignKey('auth.User') # 비추천방식
+    # user = models.OneToOneField(User) # 비추천방식
+    user = models.OneToOneField(settings.AUTH_USER_MODEL) 
+    # 추천방식
+    # 장고 사용자 인증에 사용되는 User 모델 변경을 지원
+
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
