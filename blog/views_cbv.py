1c782b65e84a9b809771a16bb68de8c7e609227e
spacemonkey <spacemonkey@spacemonkeyui-MacBook-Pro.local>
2018-06-01 21:51:08 +0900

    cbv를 활용한 new와 get_absolute_url() 을 통한 detail로 이동

---
+++ blog/views_cbv.py
@@ -0,0 +1,17 @@
+from django.views.generic import CreateView
+from django import forms
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    # success_url = '/'
+    # get_absolute_url() 을 작성해놓으면 success_url 필요치 않아.
+
+post_new = PostCreateView.as_view()
