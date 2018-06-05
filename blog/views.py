561873fa7b9f858148e11a50ee1e7faa7d7095d0
spacemonkey <spacemonkey@spacemonkeyui-MacBook-Pro.local>
2018-06-05 22:57:49 +0900

    blog.views.post_new/post_edit 뷰를 구현

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from django import forms
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -49,3 +51,28 @@ def post_detail(request, id):
     # http 500 응답
     # 서버에서 요청처리중에 예기치못한 오류(코드오류, 설정오류) 가 발생할 경우
 
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post) # post.get_absolute_url() => post detail
+    else:    
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {
+        'form':form,
+    })
+
+def post_edit(request, id):
+    post = get_object_or_404(Post, id=id)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post) # post.get_absolute_url() => post detail
+    else:    
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {
+        'form':form,
+    })
