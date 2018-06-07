84a664c8e531b4a97ee2457983afd5681b0a9efe
spacemonkey <spacemonkey@spacemonkeyui-MacBook-Pro.local>
2018-06-07 23:38:37 +0900

    filesystem static finder 설정 및 static 샘플추가

--- travel/settings.py
+++ travel/settings.py
@@ -135,7 +135,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' 
+
+# 최상위 정적인 파일의 위치설정
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'travel', 'static')
+]
 
 INTERNAL_IPS = ['127.0.0.1']
 
