991ef7b9fe6dafe8428532dffa47081559c6288b
spacemonkey <spacemonkey@spacemonkeyui-MacBook-Pro.local>
2018-06-12 15:34:47 +0900

    퀴즈를 맞춰야 로그인

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views 
 from . import views
+from .forms import LoginForm
 
 urlpatterns=[
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^login/$', auth_views.login, name='login', kwargs={'template_name':'accounts/login_form.html'}),
+    # url(r'^login/$', auth_views.login, name='login', kwargs={'template_name':'accounts/login_form.html'}),
+    url(r'^login/$', auth_views.login, name='login', kwargs={'template_name':'accounts/login_form.html','authentication_form': LoginForm}), # login form custom
     url(r'^logout/$', auth_views.logout, name='logout', kwargs={'next_page': settings.LOGIN_URL}),
     url(r'^profile/$', views.profile, name='profile'),
 
