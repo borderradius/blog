c8997320fba01ec6a5543e57645b4caf917e00af
spacemonkey <spacemonkey@spacemonkeyui-MacBook-Pro.local>
2018-06-14 16:49:57 +0900

    404, 500 error 템플릿 추가

--- travel/settings.py
+++ travel/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # ㅓㅕ'ENGINE': 'django.db.backends.mysql',
+        # 'ENGINE': 'django.db.backends.mysql',
         # 'USER': 'db user name',
         # 'PASSWORD': 'db password',
         # 'HOST': '127.0.0.1',
